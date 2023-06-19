395307fccf03ba2c9ba70b8178bd2fab2b1b94b9
hieu nguyen <nm.hieu.191@gmail.com>
2023-06-19 11:29:59 +0700

    change server url

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,9 @@ import { io } from "socket.io-client"
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-    process.env.NODE_ENV === "production" ? undefined : "http://localhost:3001"
+    process.env.NODE_ENV === "production"
+        ? process.env.REACT_APP_BACKEND_URL
+        : "http://localhost:3001"
 
 export const socket = io(URL, {
     forceNew: true,
